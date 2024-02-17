0876084b44faf8aaf2f8e6700210d083c2c4da03
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-17 15:50:03 +0400

    favicon and icons added

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,10 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="{{asset('favicon.ico')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('img/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('img/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('img/favicon-16x16.png')}}">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
