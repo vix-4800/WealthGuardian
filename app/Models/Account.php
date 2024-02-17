d27f3624bac4ff213198d409794526a87f79430c
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-17 16:29:36 +0400

    user accounts added

---
+++ app/Models/Account.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    protected $table = 'accounts';
+
+    protected $fillable = [
+        'name',
+        'user_id',
+        'bank',
+        'balance'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+}
