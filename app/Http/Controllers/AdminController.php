ef32e1146551a703e131456483c8db4b03a3443c
VIX <55950607+vix-4800@users.noreply.github.com>
2024-02-22 01:11:22 +0400

    admin controller and dashboard view added

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function dashboard()
+    {
+        return view('pages.profile.admin.dashboard');
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -6,5 +6,6 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->controller(App\Http\Controllers\ProfileController::class)->group(function () {
+])->controller(App\Http\Controllers\AdminController::class)->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
 });
