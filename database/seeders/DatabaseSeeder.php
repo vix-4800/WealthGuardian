e45f69df1bcf09bc08c0bfb9f0654881ca26559e
VIX <55950607+vix-4800@users.noreply.github.com>
2024-02-24 23:09:32 +0400

    user factory and seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $seeders = [
             SubscriptionPlanSeeder::class,
             SubscriptionPlanFeatureSeeder::class,
+            UserSeeder::class,
         ];
 
         foreach ($seeders as $seeder) {
