a5c297be6279d9fa443cb4ecda7b06a29b728d1a
VIX <55950607+vix-4800@users.noreply.github.com>
2024-02-24 23:11:09 +0400

    database trigger added - admin can not have organization

---
+++ database/migrations/2024_02_24_181143_create_admin_no_organization_trigger.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+        CREATE TRIGGER `admin_no_organization`
+        BEFORE UPDATE ON `users`
+        FOR EACH ROW
+        BEGIN
+            IF NEW.admin = 1 AND OLD.admin = 0 THEN
+                SET NEW.organization_id = NULL;
+            END IF;
+        END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS `admin_no_organization`');
+    }
+};
