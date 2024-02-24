b9be093ad92f37c6d293e2c4d659c2c474b072c2
VIX <55950607+vix-4800@users.noreply.github.com>
2024-02-24 23:00:43 +0400

    subscription blade file - minor optimization

--- resources/views/pages/profile/subscriptions/index.blade.php
+++ resources/views/pages/profile/subscriptions/index.blade.php
@@ -25,18 +25,21 @@
                                 <div x-show='showAll' x-cloak>
                                     <div class="flex gap-3">
                                         @foreach ($subscriptions as $subscription)
-                                        <div class="flex flex-col justify-between max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                        @php
+                                        $isActive = $subscription->id === $currentSubscription->id;
+                                        @endphp
+                                        <div class="flex flex-col justify-between max-w-sm p-6 bg-white border {{$isActive ? 'border-green-400' : 'border-gray-200'}}  rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                             <h5 class="flex justify-between mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                                 {{$subscription->name}}
-                                                <span class="{{$subscription->id === $currentSubscription->id ? 'border border-green-400 bg-green-100 text-green-800' : 'border border-red-400 bg-red-100 text-red-800'}} h-fit  text-xs font-medium me-2 px-2.5 py-0.5 rounded ">
-                                                    {{$subscription->id === $currentSubscription->id ? 'Active' : 'Inactive'}}
+                                                <span class="{{$isActive ? 'border border-green-400 bg-green-100 text-green-800' : 'border border-red-400 bg-red-100 text-red-800'}} h-fit  text-xs font-medium me-2 px-2.5 py-0.5 rounded ">
+                                                    {{$isActive ? 'Active' : 'Inactive'}}
                                                 </span>
                                             </h5>
                                             <p class="font-normal text-gray-700 dark:text-gray-400">
                                                 {{$subscription->description}}
                                             </p>
                                             <div class="mt-3">
-                                                @if ($subscription->id === $currentSubscription->id)
+                                                @if ($isActive)
                                                 <button type="button" disabled class="text-white bg-gray-400 cursor-not-allowed focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">
                                                     Selected
                                                 </button>
