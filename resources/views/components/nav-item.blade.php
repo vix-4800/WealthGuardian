c83c21f0f65375270cc6f282f0e10021494dfe8c
VIX <55950607+vix-4800@users.noreply.github.com>
2024-02-24 17:54:10 +0400

    Color palette changed on guest layout

--- resources/views/components/nav-item.blade.php
+++ resources/views/components/nav-item.blade.php
@@ -1,7 +1,7 @@
 @props(['name','route'])
 
 <li>
-    <a href="{{route($route)}}" class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white" aria-current="page">
+    <a href="{{route($route)}}" class="block py-2 pl-3 pr-4 text-whiterounded hover:text-purple-200 lg:bg-transparent lg:p-0" aria-current="page">
         {{$name}}
     </a>
 </li>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -41,17 +41,17 @@
 </head>
 <body>
     <header class="fixed w-full">
-        <nav class=" border-gray-200 bg-purple-100 py-2.5 dark:bg-gray-900">
+        <nav class=" border-gray-200 bg-purple-500 text-white py-2.5 dark:bg-gray-900">
             <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
                 <a href="{{route('main')}}" class="flex items-center">
                     <img src="{{asset('img/logo.png')}}" class="h-6 mr-3 rounded-md sm:h-9" alt="{{ config('app.name') }} Logo" />
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                    <span class="self-center text-xl font-semibold text-white whitespace-nowrap hover:text-purple-300">
                         {{ config('app.name') }}
                     </span>
                 </a>
                 <div class="flex items-center lg:order-2">
                     @auth
-                    <a href="{{route('dashboard')}}" class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
+                    <a href="{{route('dashboard')}}" class="text-purple-700 hover:text-purple-100 hover:shadow-lg bg-white hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
                         Profile
                     </a>
                     @else
@@ -60,15 +60,17 @@
                     </a>
                     @endauth
 
-                    <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-100 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                         <span class="sr-only">
                             Open main menu
                         </span>
                         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd">
+                            </path>
                         </svg>
                         <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd">
+                            </path>
                         </svg>
                     </button>
                 </div>
@@ -91,55 +93,56 @@
         </div>
     </main>
 
-    <footer class="bg-purple-100 dark:bg-gray-800">
-        <div class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
+    <footer class="bg-purple-500 dark:bg-gray-800">
+        <div class="max-w-screen-xl p-4 py-6 mx-auto lg:py-10 md:p-8 lg:p-10">
             <div class="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-3">
-                <div>
-                    <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                <div class="text-white">
+                    <h3 class="mb-6 text-lg font-semibold uppercase dark:text-white">
                         Me
                     </h3>
-                    <ul class="text-gray-500 dark:text-gray-400">
-                        <li class="mb-4">
-                            <a href="https://github.com/vix-4800" class=" hover:underline">
-                                About
+                    <ul class=" dark:text-gray-400">
+                        <li class="mb-4 ">
+                            <a href="https://github.com/vix-4800" class=" hover:underline hover:text-gray-900">
+                                About (GitHub profile)
                             </a>
                         </li>
                         <li class="mb-4">
-                            <a href="https://vix-4800.github.io" class="hover:underline">
-                                Blog
+                            <a href="https://vix-4800.github.io" class="hover:underline hover:text-gray-900">
+                                Website
                             </a>
                         </li>
                     </ul>
                 </div>
-                <div>
-                    <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                <div class="text-white">
+                    <h3 class="mb-6 text-lg font-semibold uppercase dark:text-white">
                         Help center
                     </h3>
-                    <ul class="text-gray-500 dark:text-gray-400">
+                    <ul class=" dark:text-gray-400">
                         <li class="mb-4">
-                            <a href="{{route('contact')}}" class="hover:underline">
+                            <a href="{{route('contact')}}" class="hover:underline hover:text-gray-900">
                                 Contact Us
                             </a>
                         </li>
                     </ul>
                 </div>
-                <div>
-                    <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+
+                <div class="text-white">
+                    <h3 class="mb-6 text-lg font-semibold uppercase dark:text-white">
                         Legal
                     </h3>
-                    <ul class="text-gray-500 dark:text-gray-400">
+                    <ul class=" dark:text-gray-400">
                         <li class="mb-4">
-                            <a href="#" class="hover:underline">
+                            <a href="{{route('policy')}}" class="hover:underline hover:text-gray-900">
                                 Privacy Policy
                             </a>
                         </li>
                         <li class="mb-4">
-                            <a href="#" class="hover:underline">
+                            <a href="{{route('license')}}" class="hover:underline hover:text-gray-900">
                                 Licensing
                             </a>
                         </li>
                         <li class="mb-4">
-                            <a href="#" class="hover:underline">
+                            <a href="{{route('terms')}}" class="hover:underline hover:text-gray-900">
                                 Terms
                             </a>
                         </li>
@@ -149,24 +152,24 @@
             <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
             <div class="text-center">
                 <div class="flex items-center justify-center">
-                    <a href="{{route('main')}}" class="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <a href="{{route('main')}}" class="flex items-center justify-center mb-5 text-2xl font-semibold text-white hover:text-purple-300 dark:text-white">
                         <img src="{{asset('img/logo.png')}}" class="h-6 mr-3 rounded-md sm:h-9" alt="{{ config('app.name') }} Logo" />
                         {{ config('app.name') }}
                     </a>
                 </div>
-                <span class="block text-sm text-center text-gray-500 dark:text-gray-400">
+                <span class="block text-sm text-center text-white dark:text-gray-400">
                     Built with
-                    <a href="https://flowbite.com" class="text-purple-600 hover:underline dark:text-purple-500">
+                    <a href="https://flowbite.com" class="text-purple-200 hover:underline dark:text-purple-500">
                         Flowbite
                     </a>
                     and
-                    <a href="https://tailwindcss.com" class="text-purple-600 hover:underline dark:text-purple-500">
+                    <a href="https://tailwindcss.com" class="text-purple-200 hover:underline dark:text-purple-500">
                         Tailwind CSS
                     </a>.
                 </span>
                 <ul class="flex justify-center mt-5 space-x-5">
                     <li>
-                        <a href="https://github.com/vix-4800/WealthGuardian" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+                        <a href="https://github.com/vix-4800/WealthGuardian" class="text-white hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
                             </svg>
