bc17b626c413d9a8fe9ac6e5f41ca2a3387a9c6a
VIX <55950607+vix-4800@users.noreply.github.com>
2024-02-26 02:05:46 +0400

    admin profile button added to layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -51,9 +51,16 @@
                 </a>
                 <div class="flex items-center lg:order-2">
                     @auth
-                    <a href="{{route('dashboard')}}" class="text-purple-700 hover:text-purple-100 hover:shadow-lg bg-white hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
+                    <a href="{{ route('admin.dashboard')}}" class="text-purple-700 hover:text-purple-100 hover:shadow-lg bg-white hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
                         Profile
                     </a>
+                    @admin
+                    @else
+                    <a href="{{ route('dashboard')}}" class="text-purple-700 hover:text-purple-100 hover:shadow-lg bg-white hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
+                        Profile
+                    </a>
+                    @endadmin
+
                     @else
                     <a href="{{route('login')}}" class="text-purple-700 hover:text-purple-100 hover:shadow-lg bg-white hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
                         Login
