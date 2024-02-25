604e3b35e1dba275bb69a453538bf74fe2846662
VIX <55950607+vix-4800@users.noreply.github.com>
2024-02-25 17:19:31 +0400

    family and organization middleware changed - now uses gates

--- app/Http/Middleware/CanHaveFamily.php
+++ app/Http/Middleware/CanHaveFamily.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Traits\HasFamily;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class CanHaveFamily
@@ -17,7 +18,11 @@ class CanHaveFamily
      */
     public function handle(Request $request, Closure $next): Response
     {
-        abort_unless(User::useTrait(HasFamily::class) && auth()->user()->canHaveFamily(), Response::HTTP_FORBIDDEN);
+        abort_unless(
+            auth()->user()->can('have_family'),
+            Response::HTTP_FORBIDDEN,
+            'You cannot have a family'
+        );
 
         return $next($request);
     }
--- app/Http/Middleware/CanHaveOrganization.php
+++ app/Http/Middleware/CanHaveOrganization.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Traits\HasOrganization;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class CanHaveOrganization
@@ -17,7 +18,11 @@ class CanHaveOrganization
      */
     public function handle(Request $request, Closure $next): Response
     {
-        abort_unless(User::useTrait(HasOrganization::class) && auth()->user()->canHaveOrganization(), Response::HTTP_FORBIDDEN);
+        abort_unless(
+            auth()->user()->can('have_organization'),
+            Response::HTTP_FORBIDDEN,
+            'You can not have an organization'
+        );
 
         return $next($request);
     }
